47c517c1fe704a52e47af7567a9a981e94cb821d
Mark Lehr <mark.lehr@rcc.edu>
2022-01-04 14:12:39 -0800

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

--- Hmwk/Assignment1/Gaddis_9thEd_Chap2_Prob20_PaintFence/main.cpp
+++ Hmwk/Assignment1/Gaddis_9thEd_Chap2_Prob20_PaintFence/main.cpp
@@ -24,6 +24,7 @@ int main(int argc, char** argv) {
     //Initialize Random Seed once here!
     
     //Declare Variables
+    //Unsigned short range -32768 to 32767
     unsigned short length,  //Length of fence in feet
                    height,  //Height of fence in feet
                  covArea,   //1 Side area of fence in feet^2
@@ -37,17 +38,18 @@ int main(int argc, char** argv) {
     cvGlPnt=340;//340 square feet coverage per gallon of paint
     
     //Map the inputs/known to the outputs
-    covArea=length*height;
+    covArea=length*height;//Surface area in ft^2, one side
     cvPaint=4*covArea;//Due to Both sides painted twice
-    nmGlPnt=cvPaint/cvGlPnt+1;
+    nmGlPnt=cvPaint/cvGlPnt+1;//Number of Gallons of Paint
     
     //Display the outputs
     cout<<"This program calculates the number of gallons of paint given:"<<endl;
-    cout<<"Fence Length = "<<length<<" feet"<<endl;
-    cout<<"Fence Height = "<<height<<" feet"<<endl;
-    cout<<"Area         = "<<covArea<<" feet^2"<<endl;
-    cout<<"Directions both sides painted twice"<<endl;
-    cout<<"Surface Area to be painted = "<<cvPaint<<" feet^2"<<endl;
+    cout<<"Fence Length                   = "<<length<<" feet"<<endl;
+    cout<<"Fence Height                   = "<<height<<" feet"<<endl;
+    cout<<"Area covered by 1 can of paint = "<<cvGlPnt<<" feet^2"<<endl;
+    cout<<"Area to be painted             = "<<covArea<<" feet^2"<<endl;
+    cout<<"Directions -> both sides painted twice"<<endl;
+    cout<<"Surface Area to be painted     = "<<cvPaint<<" feet^2"<<endl;
     cout<<"The number of gallons of paint = "<<nmGlPnt<<endl;
 
     //Exit the program
