357a562fe9f9af413589c4ec0e0dbc00c692cec7
Mark Lehr <mark.lehr@rcc.edu>
2022-01-05 10:17:34 -0800

    Signed-off-by: Mark Lehr <mark.lehr@rcc.edu>

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on January 3, 2022, 11:20 AM
+ * Purpose:  Hello World
+ */
+
+//System Level Libraries
+#include <iostream>  //Input-Output Library
+using namespace std;
+
+//User Defined Libraries
+
+//Global Constants, not Global Variables
+//These are recognized constants from the sciences
+//Physics/Chemistry/Engineering and Conversions between
+//systems of units!
+
+//Function Prototypes
+
+//Execution begins here!
+int main(int argc, char** argv) {
+    //Initialize Random Seed once here!
+    
+    //Declare Variables
+    
+    //Initialize Variables
+    
+    //Map the inputs/known to the outputs
+    
+    //Display the outputs
+    cout<<"Hello World"<<endl;
+
+    //Exit the program
+    return 0;
+}
